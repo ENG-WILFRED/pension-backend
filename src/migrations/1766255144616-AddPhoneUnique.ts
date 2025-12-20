2b1bf2c677030589a114d53451a54de84f853a79
W!LL <kimaniwilfred95@gmail.com>
2025-12-20 21:30:02 +0300

    fix: Clean up migration to only add phone unique constraint

---
+++ src/migrations/1766255144616-AddPhoneUnique.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddPhoneUnique1766255144616 implements MigrationInterface {
+    name = 'AddPhoneUnique1766255144616'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "users" ADD CONSTRAINT "UQ_a000cca60bcf04454e727699490" UNIQUE ("phone")`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "users" DROP CONSTRAINT "UQ_a000cca60bcf04454e727699490"`);
+    }
+
+}
